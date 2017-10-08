85a190423fe497fc2f0004574262dd55ec16b917
Sven Jedeck <sven@jedeck.de>
2017-10-08 06:07:29 +0200

    First step in NLog ....

--- NuGetTest/MainWindow.xaml.vb
+++ NuGetTest/MainWindow.xaml.vb
@@ -1,5 +1,16 @@
-﻿Class MainWindow
+﻿Imports NLog
+
+
+Class MainWindow
+
+Private ThisLogger As Logger = LogManager.GetCurrentClassLogger
+
+
+
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
         txtTimeStamp.Text = Now.ToLongTimeString
+
+        ThisLogger.Info("Time: " & txtTimeStamp.Text)
+
     End Sub
 End Class
